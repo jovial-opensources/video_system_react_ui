adaf29a1ab88bc085a78add8dd7fb54010840960
Rishav Bhowmik <bhowmikrishav@gmail.com>
2021-03-10 18:57:07 +0530

    git fault

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,24 @@ function appUrlParser(url:string): string{
     )
 }
 
-
-
 function App() {
 
-    const [value, setState] = useState<string>('login')
+    // eslint-disable-next-line no-restricted-globals
+    const initState: string = appUrlParser(location.pathname)
+
+    const [value, setState] = useState<string>(initState)
 
     const onLogin = ()=>{
-        // eslint-disable-next-line no-restricted-globals
         const url: string = 'login'
         setState(url)
     }
     const onRegister = ()=>{
-        // eslint-disable-next-line no-restricted-globals
         const url: string = 'register'
+        setState(url)
+    }
+
+    const onDashboard = ()=>{
+        const url: string = 'dashboard'
         setState(url)
     }
 
@@ -35,7 +39,7 @@ function App() {
 
   return (
     <div ref={root_ref}>
-      <AppHeader onLogin={onLogin} onRegister={onRegister}/>
+      <AppHeader onLogin={onLogin} onRegister={onRegister} onDashboard={onDashboard}/>
       <Main component={component} main_ref={main_ref}/>
     </div>
   );
